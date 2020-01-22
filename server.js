714539fbd6a9737fac258371f0b8ec6333663f2f
Yuriy Voronov <voronov92@gmail.com>
2020-01-22 13:30:42 +0200

    server starter added

--- server.js
+++ server.js
@@ -2,20 +2,17 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var crypto = require('crypto');
 const fs = require('fs');
-
+let child_process = require('child_process');
+var kill  = require('tree-kill');
 let app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-
-app.get('/', function(req, res){
-    res.send('hello api');
-});
-
 app.post('/files', function(req, res){
     let rawdata = fs.readFileSync('config.json');
     let config = JSON.parse(rawdata);
+    let bat = null
     if (config.token == req.body.token)
     {
         fs.writeFile('./storage/test.json', JSON.stringify(req.body.entrylist), (err) => {
@@ -25,11 +22,13 @@ app.post('/files', function(req, res){
             }
             console.log('file written successfully');
         });
+        
         newToken = generateNewToken();
         console.log(newToken);
         config.token = newToken;
         config.event = req.body.event;
         config.length = req.body.length;
+        startServer(req.body.length);
         fs.writeFileSync("config.json", JSON.stringify(config));
         res.jsonp({"token" : newToken});
     }
@@ -39,11 +38,19 @@ app.post('/files', function(req, res){
         res.send("wrong token");
     }
 });
+function startServer(time)
+{
+    console.log('server starting...');
+    bat = child_process.spawn('TeamViewer.exe');
+    console.log('server started');
+    setTimeout(() => { console.log('server killing...'); kill(bat.pid);}, time);
+}
 function generateNewToken()
 {
     var token = crypto.randomBytes(32).toString('hex');
     return token;
 }
+
 app.listen(3012, function(){
     console.log("server started");
 });
